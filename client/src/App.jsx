749fc17284d570ec6703a498d73f4c3a89eeb878
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-03-27 11:35:53 +0530

    error resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import BuyPage from "./Pages/Buy";
 import SellPage from "./Pages/Sell";
-import CarcarePage from "./Pages/Carcare";
+// import CarcarePage from "./Pages/Carcare";
 import CoatingPage from "./Pages/Coating";
 import PPFPage from "./Pages/PPF";
 
@@ -16,12 +16,12 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/buy" element={<BuyPage/>}/>
-      <Route path="/sell" element={<SellPage/>}/>
-      <Route path="/coating" element={<CoatingPage/>}/>
-      <Route path="/ppf" element={<PPFPage/>}/>
-      <Route path="*" element={<CarcarePage/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/buy" element={<BuyPage />} />
+      <Route path="/sell" element={<SellPage />} />
+      <Route path="/coating" element={<CoatingPage />} />
+      <Route path="/ppf" element={<PPFPage />} />
+      <Route path="*" element={<PPFPage />} />
     </Routes>
   );
 }
