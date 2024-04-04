e58b4c1dbf3e01d14e55f5bb27ad3019ebcbe114
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-04-04 17:54:37 +0530

    banner udpated

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,28 +1,43 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-// icons
+// Correctly import icons
 import { IoCall } from "react-icons/io5";
-import { FaWhatsapp } from "react-icons/fa6";
-import { RxCross2 } from "react-icons/rx";
+import { FaWhatsapp } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai"; // Assuming this is the close icon you intended
 
 function Banner() {
   const [tab, setTab] = useState(true);
+
   return (
     <>
-      <div className="hidden fixed bottom-10 right-10 md:flex items-center gap-2">
-        <div className="bg-white p-3 rounded-lg hover:cursor-pointer">
-          <FaWhatsapp className="text-3xl text-gray-800" />
+      {tab && (
+        <div className="hidden fixed bottom-10 right-10 md:flex flex-row items-center gap-2">
+          <a href="https://wa.me/917744900001">
+            <div className="bg-white p-3 rounded-lg hover:cursor-pointer">
+              <FaWhatsapp className="text-3xl text-gray-800" />
+            </div>
+          </a>
+          <a href="tel:917744900001">
+            <div className="bg-white p-3 rounded-lg hover:cursor-pointer">
+              <IoCall className="text-3xl text-gray-800" />
+            </div>
+          </a>
+          <div
+            className="absolute -top-5 bg-white -right-5 rounded-lg hover:cursor-pointer"
+            onClick={() => setTab(false)}
+          >
+            <AiOutlineClose className="text-gray-800 text-xl" />
+          </div>
         </div>
-        <div className="bg-white p-3 rounded-lg hover:cursor-pointer">
-          <IoCall className="text-3xl text-gray-800" />
-        </div>
-        <div className="absolute top-0" onClick={() => setTab(false)}>
-          <RxCross2 />
-        </div>
-      </div>
+      )}
+
       <div className="fixed md:hidden z-50 bottom-2 w-full px-2">
-        <button className="px-8 py-3 w-full bg-blue-800 text-white">Book Now</button>
+        <Link to="/contact-us">
+          <button className="px-8 py-3 w-full bg-red-800 text-white">
+            Book Now
+          </button>
+        </Link>
       </div>
     </>
   );
