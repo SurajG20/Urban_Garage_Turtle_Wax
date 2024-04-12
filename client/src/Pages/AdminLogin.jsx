04229ddd413b4d24bf80057e7b216ed8ffd12146
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-04-12 13:17:24 +0530

    admin Login Page added

--- client/src/Pages/AdminLogin.jsx
+++ client/src/Pages/AdminLogin.jsx
@@ -1,30 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AdminLogin() {
   return (
     <div
-      className="h-screen flex justify-center items-center bg-no-repeat bg-cover bg-center relative"
-      style={{ backgroundImage: "url('assets/logo/dark-logo.jpeg')" }}
+      className="h-screen flex justify-center items-center bg-no-repeat bg-cover bg-center relative p-4"
+      style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1551522435-a13afa10f103?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      }}
     >
-      <div className="absolute inset-0 bg-black opacity-50"></div>{" "}
+      <div className="absolute inset-0  opacity-50 bg-gray-50 blur-xl"></div>{" "}
       {/* Overlay */}
-      <div className="p-5 w-1/3 bg-white rounded-lg shadow-lg z-10">
+      <div className="p-5 w-full md:w-1/3 bg-white rounded-lg shadow-lg z-10">
         <form>
-          <div className="mb-4">
+          <div className="mb-10 ">
+            <div className="h-16 w-16 m-auto mb-5">
+              <Link to={"/"}>
+                <img
+                  className="h-full w-full object-cover object-center"
+                  src="assets/logo/light-logo.png"
+                  alt="Urban Garage"
+                />
+              </Link>
+            </div>
+            <h2 className="h2 text-theme-semibold text-center">Admin Login</h2>
+          </div>
+          <div className="mb-5">
             <label
-              htmlFor="email"
+              htmlFor="username"
               className="block text-sm font-medium text-gray-700"
             >
-              Email
+              Username
             </label>
             <input
+              placeholder="Enter Username"
               type="email"
               id="email"
               name="email"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="py-2 px-3 mt-1 bg-gray-200 ring-gray-300 focus:ring-2 focus:ring-theme-red focus:outline-none rounded-md"
             />
           </div>
-          <div className="mb-4">
+          <div className="mb-5">
             <label
               htmlFor="password"
               className="block text-sm font-medium text-gray-700"
@@ -32,11 +49,17 @@ function AdminLogin() {
               Password
             </label>
             <input
+              placeholder="Password"
               type="password"
               id="password"
               name="password"
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="py-2 px-3 mt-1 bg-gray-200 ring-gray-300 focus:ring-2 focus:ring-theme-red focus:outline-none rounded-md"
             />
+          </div>
+          <div className="mb-4">
+            <button className="bg-theme-red text-white py-2 px-5 rounded-lg">
+              Submit
+            </button>
           </div>
         </form>
       </div>
