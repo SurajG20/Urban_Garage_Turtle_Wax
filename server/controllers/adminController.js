0fe88f9b34d6e5f750fc282aaacc2bd0cd3c0784
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-04-12 20:06:57 +0530

    admin updated

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -30,30 +30,46 @@ exports.sendNotificationMail = async (req, res) => {
 };
 
 // Example Admin function
-exports.listAllAdmins = async (req, res) => {
-  console.log("List all admins");
+exports.AdminLogin = async (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
+
   try {
-    const admins = await Admin.find();
-    if (admins.length > 0) {
-      // Map through the admins to extract the IDs
-      const adminIDs = admins.map((admin) => admin._id);
-      res.json({
-        isAdmin: true, // Indicate that admins exist
-        adminIDs: adminIDs, // Send the list of admin IDs
-        admins: admins, // Optionally send full admin details
-      });
-    } else {
-      // No admins found in the database
-      res.json({
-        isAdmin: false, // Indicate that no admins exist
-        adminIDs: [], // No admin IDs to send
-        message: "No admins found",
+    // Try to find the admin by username
+    const admin = await Admin.findOne({ username: username });
+
+    if (!admin) {
+      // If no admin is found with the given username
+      return res.status(404).json({
+        isAuthenticated: false,
+        isAdmin: false,
+        message: "Admin not found",
       });
     }
+    // if Admin presend then compare the password
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) {
+      // If the passwords do not match
+      return res.status(401).json({
+        isAuthenticated: false,
+        isAdmin: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    // If the password matches, send back a success response
+    res.json({
+      isAuthenticated: true,
+      isAdmin: true, // Since the user is authenticated as an admin
+      message: "Admin authentication successful",
+      admin: {
+        id: admin._id,
+        username: admin.username,
+      },
+    });
   } catch (error) {
     console.error("Server error: ", error);
     res.status(500).json({ error: "Server error" });
   }
 };
-
 
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 
 // Assuming you have a GET method in your controller
-router.post("/", adminController.listAllAdmins);
+router.post("/", adminController.AdminLogin);
 router.post("/sendmail", adminController.sendNotificationMail);
 
 module.exports = router;
