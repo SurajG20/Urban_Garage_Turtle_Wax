9c1978aa8713f5db5e4cadacb2420978e8c298c3
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-04-15 02:35:38 +0530

    admin model updated

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,4 +1,4 @@
-const Admin = require("../models/adminModel");
+const Admin = require("../models/adminModel")
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
