d4d712bdec81a94732dc49bd9d0d9114f6298cc3
Akshit Dhakad <116799118+akshitDhakad@users.noreply.github.com>
2024-04-19 02:21:59 +0530

    baseurl updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 import { CarProvider } from "./CarsContent.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <CarProvider>
           <App />
         </CarProvider>
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
