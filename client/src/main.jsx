ca010c47e7f4c26a50d4daf3a82e1ad32ad5673d
Suraj Goswami <121960253+SurajG20@users.noreply.github.com>
2024-04-27 23:19:55 +0530

    main js updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter,HashRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
-import { CarProvider } from "./CarsContent.jsx";
+import { CarProvider } from './CarsContent.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/'>
       <QueryClientProvider client={queryClient}>
         <CarProvider>
           <App />
         </CarProvider>
       </QueryClientProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
